88c30f29848bb3640f856a34c0ad28a487e33623
Hasan Iqtedar <hasan.iqtedar@gmail.com>
2022-09-20 20:28:17 +0500

    Add dashboard page

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,72 @@
+import Card from "../components/Card";
+import "../styles/dashboard.css";
+
+const Dashboard = (props) => {
+  return (
+    <div className="dashboard">
+      <div className="cash-overview">
+        <Card>
+          <h3>Total</h3> <span>$21,360</span>
+        </Card>
+        <Card>
+          <h3>Available</h3> <span>$21,360</span>
+        </Card>
+        <Card>
+          <h3>Withdrawn</h3> <span>$21,360</span>
+        </Card>
+        <Card>
+          <h3>Clearance</h3> <span>$21,360</span>
+        </Card>
+      </div>
+      <div className="transactions-details">
+        <div className="graph-container">
+          <div className="title">
+            <h3>Today's trends</h3>
+            <span>as of 1: 32: 4</span>
+          </div>
+          <div className="graph"></div>
+        </div>
+        <div className="details">
+          <Card style={style.detailsCardStyleBorder}>
+            <h3 style={style.detailsHeadingStyle}>Total Orders</h3>{" "}
+            <span>$21,360</span>
+          </Card>
+          <Card style={style.detailsCardStyleBorderLight}>
+            <h3 style={style.detailsHeadingStyle}>Average Location Distance</h3>{" "}
+            <span>25km</span>
+          </Card>
+          <Card style={style.detailsCardStyleBorderLight}>
+            <h3 style={style.detailsHeadingStyle}>
+              Average First Response Time
+            </h3>{" "}
+            <span>33m</span>
+          </Card>
+          <Card style={style.detailsCardStyleBorder}>
+            <h3 style={style.detailsHeadingStyle}>Average Response Time</h3>{" "}
+            <span>3h 8m</span>
+          </Card>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
+
+const style = {
+  detailsCardStyleBorder: {
+    boxShadow: "none",
+    borderBottom: "2px solid lightgray",
+    borderRadius: "0",
+    width: "150px",
+  },
+  detailsCardStyleBorderLight: {
+    boxShadow: "none",
+    borderBottom: "1px solid #f5f4f4",
+    borderRadius: "0",
+    width: "150px",
+  },
+  detailsHeadingStyle: {
+    fontSize: "12px",
+  },
+};
