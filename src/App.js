dc26fbb5152a9ce48d072caecd67fbf4615e6fc4
Hasan Iqtedar <hasan.iqtedar@gmail.com>
2022-09-22 21:23:05 +0500

    Add Global State

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
 import { Route, Routes, Navigate } from "react-router-dom";
+import { GlobalProvider } from "./contexts/globalState";
 import NavigationPanel from "./components/NavigationPanel";
 import Container from "./components/Container";
 import Header from "./components/Header";
 import Settings from "./pages/Settings";
 import UserForm from "./pages/UserForm";
 import Users from "./pages/Users";
-import "./styles/app.css";
 import Dashboard from "./pages/Dashboard";
+import "./styles/app.css";
 
 function App() {
   return (
-    <div className="app">
-      <NavigationPanel />
-      <Container>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Navigate to="/dashboard" replace />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/users" element={<Users />} />
-          <Route path="/settings" element={<Settings />} />
-          <Route path="/add-user" element={<UserForm />} />
-        </Routes>
-      </Container>
-    </div>
+    <GlobalProvider>
+      <div className="app">
+        <NavigationPanel />
+        <Container>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Navigate to="/dashboard" replace />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/users" element={<Users />} />
+            <Route path="/settings" element={<Settings />} />
+            <Route path="/add-user" element={<UserForm />} />
+          </Routes>
+        </Container>
+      </div>
+    </GlobalProvider>
   );
 }
 
