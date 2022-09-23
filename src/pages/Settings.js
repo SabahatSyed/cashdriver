31ec8a255fea2300b27877dac462fe8d19aea04b
Hasan Iqtedar <hasan.iqtedar@gmail.com>
2022-09-23 21:34:30 +0500

    Allow update of admin data

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,9 +1,44 @@
-import { IoLockClosedOutline, IoPencil } from "react-icons/io5";
-// import profileIcon from "../assets/profilePictureIcon.png";
+import { useState } from "react";
+import { doc, updateDoc } from "firebase/firestore";
+import { db } from "./firebase";
+// import { IoLockClosedOutline, IoPencil } from "react-icons/io5";
 import "../styles/settings.css";
-// import switchImage from "../assets/switchImage.png";
+import { useGlobalState } from "../contexts/globalState";
 
 const Settings = (props) => {
+  const { adminCredentials, updateAdminPassword, updateAdminWalletKey } =
+    useGlobalState();
+  const [password, setPassword] = useState("");
+  const [adminKey, setAdminKey] = useState("");
+
+  const updatePassword = async () => {
+    if (password !== "") {
+      const adminRef = doc(db, "admin", adminCredentials.id);
+      try {
+        await updateDoc(adminRef, {
+          password: password,
+        });
+        updateAdminPassword(password);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  };
+
+  const updateKey = async () => {
+    if (adminKey !== "") {
+      const adminRef = doc(db, "admin", adminCredentials.id);
+      try {
+        await updateDoc(adminRef, {
+          walletKey: adminKey,
+        });
+        updateAdminWalletKey(adminKey);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  };
+
   return (
     <div className="settings">
       <h2 className="settings-title">Settings</h2>
@@ -11,12 +46,26 @@ const Settings = (props) => {
         <h3>Security</h3>
         <div className="settings-fields-container">
           <div className="field">
-            <input placeholder="Update Password" />
-            <button className="update-btn">Update</button>
+            <input
+              placeholder="Update Password"
+              name="adminPassword"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button className="update-btn" onClick={updatePassword}>
+              Update
+            </button>
           </div>
           <div className="field" style={{ borderBottom: "none" }}>
-            <input placeholder="Update Admin Wallet Key" />
-            <button className="update-btn">Update</button>
+            <input
+              placeholder="Update Admin Wallet Key"
+              name="adminKey"
+              value={adminKey}
+              onChange={(e) => setAdminKey(e.target.value)}
+            />
+            <button className="update-btn" onClick={updateKey}>
+              Update
+            </button>
           </div>
         </div>
       </div>
